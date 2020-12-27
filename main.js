b47179b14976b3e47ceb7f3099d367bd1a9ac4ce
Praveen Nair <prnair@ucsd.edu>
2020-12-26 22:57:57 -0800

    Working with click to reveal neighbors

---
+++ main.js
@@ -0,0 +1,11 @@
+sigma.classes.graph.addMethod('neighbors', function(nodeId) {
+    var k,
+        neighbors = {},
+        index = this.allNeighborsIndex[nodeId] || {};
+
+    for (k in index)
+      neighbors[k] = this.nodesIndex[k];
+
+    return neighbors;
+  });
+
